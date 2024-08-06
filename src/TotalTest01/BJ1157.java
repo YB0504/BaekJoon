59d5fb06e5ffc4d58ad451bd9277498ac62497da
INBEOM YEO <yeo9411@naver.com>
2024-08-06 13:09:04 +0900

    백준 심화1 문제 주석 공부02

--- src/TotalTest01/BJ1157.java
+++ src/TotalTest01/BJ1157.java
@@ -6,10 +6,15 @@ public class BJ1157 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // 알파벳의 사용 여부 판단을 위한 배열 선언
         int[] arr = new int[26];
         String s = scanner.next();
 
+        // 입력받은 문자열의 길이 만큼 반복
         for (int i = 0; i < s.length(); i++) {
+            // 대소문자 구별이 없다.
+            // 각 문자의 ASCII코드 값에서 'A', 'a'를 빼서
+            // 해당 문자의 인덱스를 도출해낸다.
             if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') {
                 arr[s.charAt(i) - 'A']++;
             } else {
@@ -17,14 +22,21 @@ public class BJ1157 {
             }
         }
 
+        // 알파벳의 빈도수는 항상 0이상이므로
+        // 빈도수가 0이어도 max가 갱신 될 수 있도록 -1로 초기화
         int max = -1;
         char ch = '?';
 
         for (int i = 0; i < 26; i++) {
+            // max가 앞서 도출해낸 index보다 작다면 해당 값을 max에 갱신하고
+            // 대문자로 출력하기 위해 ASCII코드 값에 65를 더해준다.
             if (arr[i] > max) {
                 max = arr[i];
                 ch = (char) (i + 65);
             } else if (arr[i] == max) {
+                // index값과 max값이 동일하다면
+                // 같은 빈도수의 알파벳이 둘 이상이라는 의미이기 때문에
+                // '?'를 출력한다.
                 ch = '?';
             }
         }
--- src/TotalTest01/BJ2941.java
+++ src/TotalTest01/BJ2941.java
@@ -8,14 +8,21 @@ public class BJ2941 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 비교할 문자열 입력 받기
         String s = br.readLine();
+        
+        // 변환 되어서 비교해야하는 크로아티안 알파벳 배열 생성
         String[] croatianAlphabet = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
         int index = 0;
 
+        // 배열 길이 만큼 반복문 순회
         for (int i = 0; i < croatianAlphabet.length; i++) {
+            // 입력받은 문자열에 크로아티안 알파벳이 있는지 확인
             index = s.indexOf(croatianAlphabet[i]);
+            // index가 0보다 크거나 같다면 배열과 같은 내용이 있다는 의미
             if (index >= 0) {
+                // 크로아티안 알파벳을 "A"로 변환하여 입력받은 문자열을 변환
                 s = s.replaceAll(croatianAlphabet[i], "A");
             }
         }
